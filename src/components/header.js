3de31df8e383ed856abca067b4e8d3ec1fa9e175
cooke001 <me@edwardcooke.co.uk>
2021-03-30 00:02:25 +0100

    Add navbar attempt

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,48 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <header>
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+        maxWidth: 960,  
+        }}
+      >
+      <h1 style={{ 
+        margin: '0 auto'
+        }}>
+        <nav>
+          <ul> 
+            <li><a href="/" style={{
+              color: `white`,
+              textDecoration: `none`,
+              float: 'left'
+              }}>
+                {siteTitle}
+            </a></li> 
+            <li><a href="/about" style={{
+              color: `white`,
+              textDecoration: `none`,
+              float: 'right',
+              }}>
+                About
+            </a></li>   
+            <li><a href="/projects" style={{
+              color: `white`,
+              textDecoration: `none`,
+              float: 'right',
+              }}>
+                Projects
+            </a></li> 
+          </ul> 
+          <select> 
+            <option value="" selected="selected">Edward Cooke</option> 
+            <option value="/">Home</option> 
+            <option value="/about">About</option> 
+            <option value="/projects">Projects</option> 
+        </select> 
+        </nav>
       </h1>
     </div>
   </header>
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,37 +1,31 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
 import Header from "./header"
 import Footer from "./footer"
 import "./layout.css"
+import { useStaticQuery, graphql } from "gatsby"
+
+
+
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
       }
     }
-  `)
-
+  }
+`)
+// 
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          maxWidth: 960, 
         }}
       >
         <main>{children}</main>
