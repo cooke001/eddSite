0b57319a3bf38da94281f9889badc3821d736364
cooke001 <me@edwardcooke.co.uk>
2021-03-30 01:07:52 +0100

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,7 +21,7 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
-          margin: `0 auto`,
+          margin: '0 auto',
           maxWidth: 960, 
         }}
       >
