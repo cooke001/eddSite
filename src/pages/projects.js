0058f2b9773bf4dface144af68a911c1962a3337
cooke001 <me@edwardcooke.co.uk>
2021-05-10 23:08:12 +0100

    Add carousel for first images
    Need to fix static image site issue....

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { StaticImage } from "gatsby-plugin-image"
 import { Row, Col } from "reactstrap"
+import Carousel from 'react-bootstrap/Carousel'
 
 const Projects = () => (
   <Layout>
@@ -22,14 +23,31 @@ const Projects = () => (
         <p>Includes hand cut and laser cut mother of pearl inlays.</p>
       </Col>
       <Col xs="4">
-        <StaticImage
-          src="../images/Monopoly/Mono1.jpeg" 
-          alt="Monopoly"    
-          style={{ width: 300, height: 300 }}
-        />
+      <Carousel slide prevLabel nextLabel>
+        <Carousel.Item>
+          <StaticImage
+            src="../images/Monopoly/Mono1.jpeg"
+          />
+        </Carousel.Item> 
+        <Carousel.Item>
+          <StaticImage
+            src="../images/Monopoly/Mono2.jpeg"
+          />
+        </Carousel.Item> 
+        <Carousel.Item>
+          <StaticImage
+            src="../images/Monopoly/Mono3.jpeg"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <StaticImage
+            src="../images/Monopoly/Mono4.jpeg"
+          />
+        </Carousel.Item>
+      </Carousel>
       </Col>
     </Row>
-    <p>To add:</p>
+    <h2>To add:</h2>
     <p>Nixie Clock</p>
     <p>Monopoly Board</p>
     <p>Mini Theatre Project</p>
